3b3961e863fcd4acd3c0b9d6051aff43ee65cae9
Nastya <nasty.patutina@gmail.com>
2023-08-07 01:58:50 +0300

    Подредактировала верстку cards и cardlist. Добавила скрытие и раскрытие  текста при нажатии

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import styles from "./Card.module.scss";
 
 const Card = ({ title, date, details, image }) => {
+  const [descShort, setDescShort] = useState(true);
   const intlDate = new Intl.DateTimeFormat("ru-RU").format(new Date(date));
   return (
     <div className={styles.card}>
       <h1 className={styles.title}>{title}</h1>
       <h2 className={styles.subtitle}>{intlDate}</h2>
       <div>
-        <p className={styles.desc}>{details}</p>
+        <p
+          onClick={() => setDescShort(!descShort)}
+          className={descShort ? styles.descShort : styles.desc}
+        >
+          {details}
+        </p>
       </div>
       <img className={styles.imgCard} src={image} alt="" />
     </div>
